359e6792e54b2a2166d842a6dc2ac650c2a618a5
the-supratim <122669346+the-supratim@users.noreply.github.com>
2023-08-26 00:17:34 +0530

    Add files via upload

---
+++ yt_downloader.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ",yt.title)
+
+print("Views: ",yt.views)
+
+yd = yt.streams.get_highest_resolution()
+
+yd.download("D:\YouTube Downloader")
+
+print("Thank you for using this software.")
