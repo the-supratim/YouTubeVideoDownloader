d0bb7abdd08fde68b8ba1a04989e244ac7380016
the-supratim <122669346+the-supratim@users.noreply.github.com>
2023-09-02 06:10:13 +0530

    Code

--- yt_downloader.py
+++ yt_downloader.py
@@ -3,7 +3,7 @@ from pytube import YouTube
 link = input("Enter link of YouTube Video: ")
 yt = YouTube(link)
 
-print("Title: ",yt.title)
+print("\nTitle: ",yt.title)
 
 print("Views: ",yt.views)
 
@@ -11,4 +11,5 @@ yd = yt.streams.get_highest_resolution()
 
 yd.download("D:\YouTube Downloader")
 
+print("\nYour file has been downloaded successfully.")
 print("Thank you for using this software.\n© Supratim Bhattacharjee")
